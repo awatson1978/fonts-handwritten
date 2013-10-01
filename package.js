763238c981101fb28d9c4fc0f693a601a7697ec8
Abigail Watson <awatson1978@gmail.com>
2013-10-01 18:49:01 -0400

    removed register_extention api call

--- package.js
+++ package.js
@@ -2,17 +2,6 @@ Package.describe({
   summary: "Meteorite package for adding handwritten script fonts to Meteor apps (not cursive fonts)."
 });
 
-Package.register_extension(
-  "ttf", function (bundle, source_path, serve_path, where) {
-    bundle.add_resource({
-      type: "static",
-      path: '/fonts/' + serve_path.split('/').pop(),
-      source_file: source_path,
-      where: where
-    });
-  }
-);
-
 Package.on_use(function (api) {
     api.add_files('fonts/ChalkDust.ttf', "client");
     api.add_files('fonts/Freehand.ttf', "client");
